58ec8701b0db36d193aa222186b69251ef171454
kumami-tech <sk924731@gmail.com>
2020-10-22 18:57:07 +0900

    [update]Movie.jsxのリファクタリング

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,16 +3,15 @@ import Dialog from '@material-ui/core/Dialog';
 
 import ModalVideo from 'react-modal-video'
 
-export default class Movie extends React.Component {
-
-  render() {
-    return(
-      <Dialog
-        open={this.props.open}
-        onClose={this.props.handleClose}
-      >
-        <ModalVideo channel='youtube' isOpen={this.props.open} videoId={this.props.videoId} onClose={() => this.setState({open: false})} />
-      </Dialog>
-    )
-  }
+const Movie = (props) => {
+  return(
+    <Dialog
+      open={props.open}
+      onClose={props.handleClose}
+    >
+      <ModalVideo channel='youtube' isOpen={props.open} videoId={props.videoId} />
+    </Dialog>
+  )
 }
+
+export default Movie
